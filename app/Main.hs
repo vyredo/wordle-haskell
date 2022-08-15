d16814abcfd146713a6a18bd792b6f8e890353ef
vidy <vidy.alfredo1@gmail.com>
2022-08-15 21:56:18 +0800

    refactor to StateMonad

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Main where
 
-import Control.Monad.Except (ExceptT, runExceptT, throwError)
+import Control.Monad.Except (ExceptT, MonadError, runExceptT, throwError)
 import Control.Monad.State
+  ( MonadIO (..),
+    MonadState (get, put),
+    modify,
+    runState,
+  )
 import Data.List (elemIndices)
 import GameStateLib (assignColors, is5LetterWord)
 import GuessLib (Guess (..), prettyPrint)
@@ -19,6 +26,7 @@ data GameState = GameState
     win :: Bool
   }
 
+gs :: GameState
 gs =
   GameState
     { maxAttempt = 5,
@@ -37,30 +45,32 @@ main =
     putStrLn (color Blue " Blue when the letter of guess is incorrect position but is part of answer")
     putStrLn (color Black " Black when the letter of guess is not part of answer word")
     word <- getRandomWords -- use Map and big list of random words
-    loop gs {answer = word}
+    let (_, s) = runState loop gs {answer = word}
+    pure ()
 
-loop :: GameState -> IO ()
-loop gs = do
+loop :: (MonadState GameState m, MonadIO m) => m ()
+loop = do
   if gameOver gs || win gs
     then pure ()
     else do
       safeInput <- runExceptT getInput
-      let (_, s) = runState stateLogic gs {userInput = safeInput}
-      logState s
-      loop s
+      put $ gs {userInput = safeInput}
+      stateLogic
+      logState
+      loop
 
-getInput :: ExceptT WordValidityError IO String
+getInput :: (MonadError WordValidityError m, MonadIO m) => m String
 getInput = do
   liftIO $ putStrLn ""
   liftIO $ putStrLn "Enter a guess:"
   line <- liftIO $ getLine >>= isWordValid
   case line of
     Left err -> do
-      -- mapM_ (putStrLn . prettyPrint) $ "what is line" ++ show err
+      -- liftIO $ putStrLn $ "what is line" ++ show err
       throwError err
     Right word -> pure word
 
-stateLogic :: State GameState ()
+stateLogic :: (MonadState GameState m, MonadIO m) => m ()
 stateLogic = do
   state <- get
   put (state {getError = ""})
@@ -72,22 +82,18 @@ stateLogic = do
       let gameOver = length (guesses state) >= maxAttempt state
       let g = checkGuessAndAssignColor guess (answer state)
       modify (\gs -> gs {guesses = guesses gs ++ [g], win = win, gameOver = gameOver})
+  where
+    checkGuessAndAssignColor :: String -> String -> [Guess]
+    checkGuessAndAssignColor guess answer =
+      if guess == answer
+        then map (\char -> Guess {getColor = Green, getCh = char}) answer
+        else assignColors answer guess
 
-checkGuessAndAssignColor :: String -> String -> [Guess]
-checkGuessAndAssignColor guess answer =
-  if guess == answer
-    then map (\char -> Guess {getColor = Green, getCh = char}) answer
-    else assignColors answer guess
-
-printGuesses :: GameState -> IO ()
-printGuesses gs = do
-  putStrLn "Your guesses:"
-  mapM_ (putStrLn . prettyPrint) $ guesses gs
-
-logState :: GameState -> IO ()
-logState s = do
-  putStrLn ""
-  log s
+logState :: (MonadState GameState m, MonadIO m) => m ()
+logState = do
+  liftIO $ putStrLn ""
+  s <- get
+  liftIO $ log s
   where
     log s
       | getError s /= [] = do
@@ -98,3 +104,8 @@ logState s = do
       | win s = putStrLn $ "You Win, the answer is " ++ answer s
       | otherwise = do
         printGuesses s
+      where
+        printGuesses :: GameState -> IO ()
+        printGuesses gs = do
+          putStrLn "Your guesses:"
+          mapM_ (putStrLn . prettyPrint) $ guesses gs
